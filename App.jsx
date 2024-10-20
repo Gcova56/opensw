712d8b6f773b133f079aa478ccb2958100a8ba7d
Gcova56 <162546872+Gcova56@users.noreply.github.com>
2024-10-20 20:13:38 +0900

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,24 @@
+import Viewer from "./Viewer";
+import Controller from "./Controller";
+import './App.css';
+import { useState } from "react";
+
+function App(){
+    const [count, setCount] = useState(0);
+    function handleSetCount(value) {
+        setCount(count+value);
+    };
+
+    return(
+        <div className="App">
+           <h1>Counter App</h1>
+           <div>
+            <Viewer count = {count}></Viewer>
+           </div>
+           <div>
+            <Controller handleSetCount = {handleSetCount}></Controller>
+           </div>
+        </div>
+    );
+};
+export default App;
